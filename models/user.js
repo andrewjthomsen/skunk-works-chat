546b311b6cf582b7645bb79b09d09137bdfed7ec
Darren Victoriano <darren.victoriano@gmail.com>
2019-03-23 12:53:24 -0700

    added new models for chat and userInfo tables

--- models/user.js
+++ models/user.js
@@ -1,8 +1,17 @@
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define("User", {
     username: DataTypes.TEXT,
     password: DataTypes.TEXT,
     email: DataTypes.TEXT
   });
+
+  User.associate = function (models) {
+    models.User.hasMany(models.Chat);
+  };
+
+  User.associate = function (models) {
+    models.User.hasOne(models.userInfo);
+  };
+
   return User;
 };
---
+++ models/userInfo.js
@@ -0,0 +1,18 @@
+module.exports = function (sequelize, DataTypes) {
+  var userInfo = sequelize.define("userInfo", {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    bio: DataTypes.TEXT
+  });
+
+  userInfo.associate = function (models) {
+    models.userInfo.belongsTo(models.User, {
+      onDelete: "CASCADE",
+      foreignKey: {
+        allowNull: false
+      }
+    });
+  };
+
+  return userInfo;
+};
